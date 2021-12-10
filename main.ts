0cbfb6ea5207e163de9d740c162609fb83c0bb01
gravygamerx <81331001+gravygamerx@users.noreply.github.com>
2021-12-10 17:10:38 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -11,12 +11,18 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     snail.y += 16
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
-if(heldBerry != null) {
-    tiles.placeOnTile(heldBerry, location)
-    heldBerry.setImage(img``)
-    heldBerry = null
-}
+    if(heldBerry != null) {
+        tiles.placeOnTile(heldBerry, location)
+        heldBerry.setImage(img``)
+        heldBerry = null
+
+        if (berriesLeft == 0) {
+            game.over(true)
+
+        }
+    }
 })
+
 function spawnBerries (numBerries: number, startCalumn: number, startRow: number, gap: number) {
     for (let index = 0; index < numBerries; index++) {
         berry = sprites.create(img`
@@ -46,24 +52,25 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (playersprite, be
     if (heldBerry == null) {
         heldBerry = berrySprite
         berrySprite.setImage(img`
-            . . . . . . . 6 . . . . . . . .
-            . . . . . . 8 6 6 . . . 6 8 . .
-            . . . e e e 8 8 6 6 . 6 7 8 . .
-            . . e 2 2 2 2 e 8 6 6 7 6 . . .
-            . e 2 2 4 4 2 7 7 7 7 7 8 6 . .
-            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 .
-            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . .
-            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 .
-            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 .
-            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 .
-            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 .
-            e 2 2 2 2 2 2 2 4 e 2 e e c . .
-            e e 2 e 2 2 4 2 2 e e e c . . .
-            e e e e 2 e 2 2 e e e c . . . .
-            e e e 2 e e c e c c c . . . . .
-            . c c c c c c c . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
         `)
     }
+    berriesLeft -=1
 })
 let startCalumn = 0
 let berry: Sprite = null
@@ -141,5 +148,12 @@ game.onUpdateInterval(500, function () {
     rightcar.setFlag(SpriteFlag.DestroyOnWall, true)
     rightcar.x = -20
 })
+let berriesLeft = 4
 
-spawnBerries(4,  2,  1,  1,)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite: Sprite, othersprite) {
+    info.changeLifeBy(-1)
+    othersprite.destroy()
+})
+
+spawnBerries(berriesLeft,  2,  1,  1,)
+info.setLife(2)
